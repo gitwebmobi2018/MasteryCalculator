e411cfc5823f4c6c04f9ceea89b85953c9e895ad
abdul ahad <abdul_ahad1996@live.com>
2019-04-06 03:34:10 +0500

    solving the autoconstaint issues

--- FinacialAnalyzingApp/MyTabBarController.swift
+++ FinacialAnalyzingApp/MyTabBarController.swift
@@ -12,10 +12,50 @@ class MyTabBarController: UITabBarController {
     
     let btnWidth = DataManagement.sharedInstance.windowSize.width/3
     
-    var viewTabBar = UIView ()
-    var greenBtn = UIButton()
-    var yellowBtn = UIButton()
-    var purpleBtn = UIButton()
+//    var viewTabBar:UIView = {
+//        let viewTabBar = UIView()
+//        viewTabBar.backgroundColor = UIColor.clear
+//        viewTabBar.translatesAutoresizingMaskIntoConstraints = false
+//
+//        return viewTabBar
+//    }()
+//    var viewTabBar = UIView()
+    var greenBtn : UIButton = {
+        let greenBtn = UIButton()
+        greenBtn.setTitle("Flip", for: .normal)
+        greenBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        greenBtn.backgroundColor = UIColor.clear
+        greenBtn.tintColor = UIColor.clear
+        greenBtn.setBackgroundImage(UIImage(named: "green.png"), for: .normal)
+        greenBtn.setTitleColor(UIColor.black, for: .normal)
+        greenBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
+        greenBtn.addTarget(self, action: #selector(toFlip), for: UIControl.Event.init(rawValue: 3))
+
+        return greenBtn
+    }()
+    
+    var yellowBtn : UIButton = {
+        let yellowBtn = UIButton()
+        yellowBtn.setTitle("Rental", for: UIControl.State.init(rawValue: 0))
+        yellowBtn.setBackgroundImage(UIImage(named: "yellow.png"), for: .normal)
+        yellowBtn.setTitleColor(UIColor.lightGray, for: .normal)
+        yellowBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
+        yellowBtn.addTarget(self, action: #selector(toRental), for: UIControl.Event.init(rawValue: 3))
+        yellowBtn.backgroundColor = UIColor.clear
+        yellowBtn.tintColor = UIColor.clear
+        return yellowBtn
+    }()
+    
+    var purpleBtn : UIButton = {
+        let purpleBtn = UIButton()
+        purpleBtn.setTitle("Rehab", for: .normal)
+        purpleBtn.setBackgroundImage(UIImage(named: "purple.png"), for: .normal)
+        purpleBtn.setTitleColor(UIColor.lightGray, for: .normal)
+        purpleBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
+        purpleBtn.addTarget(self, action: #selector(toRehab), for: UIControl.Event.init(rawValue: 3))
+        
+        return purpleBtn
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,40 +69,52 @@ class MyTabBarController: UITabBarController {
     }
     
     func initView() {
+     
+//
+//        viewTabBar = UIView(frame: CGRect(x: 0, y: DataManagement.sharedInstance.windowSize.height - 60, width: DataManagement.sharedInstance.windowSize.width, height: 60))
+//
+//        viewTabBar.backgroundColor = UIColor.clear
+        let stackView = UIStackView(arrangedSubviews: [greenBtn,yellowBtn,purpleBtn])
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        view.addSubview(stackView)
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 40).isActive = true 
         
-        viewTabBar = UIView(frame: CGRect(x: 0, y: DataManagement.sharedInstance.windowSize.height - 49, width: DataManagement.sharedInstance.windowSize.width, height: 49))
-        viewTabBar.backgroundColor = UIColor.clear
+//        greenBtn = UIButton(frame: CGRect(x: 0, y: 0, width: DataManagement.sharedInstance.windowSize.width/3, height: 60))
+//        greenBtn.setTitle("Flip", for: .normal)
+//        greenBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+//        greenBtn.backgroundColor = UIColor.clear
+//        greenBtn.tintColor = UIColor.clear
+//        greenBtn.setBackgroundImage(UIImage(named: "green.png"), for: .normal)
+//        greenBtn.setTitleColor(UIColor.black, for: .normal)
+//        greenBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
+//        greenBtn.addTarget(self, action: #selector(toFlip), for: UIControl.Event.init(rawValue: 3))
         
-        greenBtn = UIButton(frame: CGRect(x: 0, y: 0, width: DataManagement.sharedInstance.windowSize.width/3, height: 49))
-        greenBtn.setTitle("Flip", for: .normal)
-        greenBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        greenBtn.backgroundColor = UIColor.clear
-        greenBtn.tintColor = UIColor.clear
-        greenBtn.setBackgroundImage(UIImage(named: "green.png"), for: .normal)
-        greenBtn.setTitleColor(UIColor.black, for: .normal)
-        greenBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
-        greenBtn.addTarget(self, action: #selector(toFlip), for: UIControl.Event.init(rawValue: 3))
+//        yellowBtn = UIButton(frame: CGRect(x: DataManagement.sharedInstance.windowSize.width/3, y: 0, width: DataManagement.sharedInstance.windowSize.width/3, height: 60))
+//        yellowBtn.setTitle("Rental", for: UIControl.State.init(rawValue: 0))
+//        yellowBtn.setBackgroundImage(UIImage(named: "yellow.png"), for: .normal)
+//        yellowBtn.setTitleColor(UIColor.lightGray, for: .normal)
+//        yellowBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
+//        yellowBtn.addTarget(self, action: #selector(toRental), for: UIControl.Event.init(rawValue: 3))
+//        yellowBtn.backgroundColor = UIColor.clear
+//        yellowBtn.tintColor = UIColor.clear
         
-        yellowBtn = UIButton(frame: CGRect(x: DataManagement.sharedInstance.windowSize.width/3, y: 0, width: DataManagement.sharedInstance.windowSize.width/3, height: 49))
-        yellowBtn.setTitle("Rental", for: UIControl.State.init(rawValue: 0))
-        yellowBtn.setBackgroundImage(UIImage(named: "yellow.png"), for: .normal)
-        yellowBtn.setTitleColor(UIColor.lightGray, for: .normal)
-        yellowBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
-        yellowBtn.addTarget(self, action: #selector(toRental), for: UIControl.Event.init(rawValue: 3))
-        yellowBtn.backgroundColor = UIColor.clear
-        yellowBtn.tintColor = UIColor.clear
-        
-        purpleBtn = UIButton(frame: CGRect(x: DataManagement.sharedInstance.windowSize.width*2/3, y: 0, width: DataManagement.sharedInstance.windowSize.width/3, height: 49))
-        purpleBtn.setTitle("Rehab", for: .normal)
-        purpleBtn.setBackgroundImage(UIImage(named: "purple.png"), for: .normal)
-        purpleBtn.setTitleColor(UIColor.lightGray, for: .normal)
-        purpleBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
-        purpleBtn.addTarget(self, action: #selector(toRehab), for: UIControl.Event.init(rawValue: 3))
-        
-        viewTabBar.addSubview(greenBtn)
-        viewTabBar.addSubview(yellowBtn)
-        viewTabBar.addSubview(purpleBtn)
-        self.view.addSubview(viewTabBar)
+//        purpleBtn = UIButton(frame: CGRect(x: DataManagement.sharedInstance.windowSize.width*2/3, y: 0, width: DataManagement.sharedInstance.windowSize.width/3, height: 60))
+//        purpleBtn.setTitle("Rehab", for: .normal)
+//        purpleBtn.setBackgroundImage(UIImage(named: "purple.png"), for: .normal)
+//        purpleBtn.setTitleColor(UIColor.lightGray, for: .normal)
+//        purpleBtn.setTitleColor(UIColor.white, for: UIControl.State.init(rawValue: 1))
+//        purpleBtn.addTarget(self, action: #selector(toRehab), for: UIControl.Event.init(rawValue: 3))
+//
+//        viewTabBar.addSubview(greenBtn)
+//        viewTabBar.addSubview(yellowBtn)
+//        viewTabBar.addSubview(purpleBtn)
+//        self.view.addSubview(viewTabBar)
     }
     @objc func toFlip() {
         self.selectedIndex = 0
